ad2392b5f549cb8cea1662412eb3928604997fa2
mandi andrade <mandi.andrade20@gmail.com>
2023-03-25 15:50:34 -0500

    empy array for collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,5 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+console.log('Here is an empty array:', collection);
+//this was the first feature; checking console that it's working
