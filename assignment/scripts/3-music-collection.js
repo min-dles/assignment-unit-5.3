97ae1b01708c6191104acfd41ac0a13a84ae586e
mandi andrade <mandi.andrade20@gmail.com>
2023-03-25 19:02:50 -0500

    2nd feature: album function first iteration

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,3 +3,18 @@ console.log('***** Music Collection *****')
 let collection = [];
 console.log('Here is an empty array:', collection);
 //this was the first feature; checking console that it's working
+
+function addToCollection(title, artist, yearPublished) {
+    console.log('Testing this function:');
+    let album = {
+        title: '',
+        artist: '',
+        yearPublished: ''
+    }; // end object 
+    collection.push(album);
+    return album;
+}
+
+console.log(addToCollection('Citrine', 'Hayley Kiyoko', '2016'));
+console.log(collection);
+// this is my first iteration of the function; it didn't work
